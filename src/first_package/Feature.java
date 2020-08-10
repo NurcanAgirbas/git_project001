b6bad83d6ab29f798e973e198da56dba07e1b2ab
your name <nurcanagirbas1@gmail.com>
2020-08-10 12:03:51 -0400

    the third commit from local master

--- src/first_package/Feature.java
+++ src/first_package/Feature.java
@@ -5,8 +5,9 @@ public class Feature {
 	public static void main(String[] args) {
 	
 		System.out.println("This is my commit from feature branch");
-                System.out.println("This is the first commit from remote master branch");
-	       System.out.println("This is the second commit removed master branch");
+        System.out.println("This is the first commit from remote master branch");
+        System.out.println("This is the second commit removed master branch");
+        System.out.println("This is the second third commit from local master");
 	}
 
 }
