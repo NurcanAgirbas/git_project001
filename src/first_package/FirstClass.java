5aba6b7890489cf6dc8db5dc56e2cb65e6189cf6
your name <nurcanagirbas1@gmail.com>
2020-08-10 09:18:31 -0400

    last commit from local master branch

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -7,6 +7,9 @@ public class FirstClass {
 		System.out.println("Our firts commit from the local repo");
         System.out.println("Our second commit from local repository");
         System.out.println("I am in nurcan branch");
+        System.out.println("I want to have conflict");
+        
+        System.out.println("I want to have a second conflict");
 
 	}
 
